ec99bba9f234f7d9c028f596f3b042c3f7c042cf
gulshanyadav01 <gulshany01@gmail.com>
2021-10-12 22:33:11 +0530

    button has been changed

--- src/components/Search.js
+++ src/components/Search.js
@@ -28,11 +28,11 @@ const  Search = () =>{
     }
     return (
         <div style = {{backgroundColor:"#0E1122"}} >
-            <div className = " h-12 rounded focus:ring-1 focus:ring-red-400  mr-4 ml-4 mb-4 flex" style = {{backgroundColor:"#182037"}}>
+            <div className = " h-12  rounded focus:ring-1 focus:ring-red-400  mr-4 ml-4 mb-4 flex" style = {{backgroundColor:"#182037"}}>
                 <AiOutlineSearch className = "ml-20 mt-4 w-8 h-8 text-blue-500"/>
                 <form className = "mt-1" onSubmit = {onSubmit}>
                     <input type = "text" placeholder = "Enter Github User" className = "bg-transparent   pl-8 text-white text-xl pr-96 h-8" sytle = {{color:"#8B98C1"}} value = {user} onChange = {onChange}/>
-                   {requests>0 && <button type = "submit" className = "ml-56  px-6 py-2 text-blue-700 ring-2 ring-yellow-300  font-extrabold rounded bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500  ">Search</button>} 
+                   {requests>0 && <button type = "submit" className = "ml-96  px-6 py-2 text-blue-700 ring-2 ring-yellow-300  font-extrabold rounded bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500  ">Search</button>} 
                 </form>
             </div>
         </div>
